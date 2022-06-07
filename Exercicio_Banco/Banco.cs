8b205774b056e54e1f55bd677d9dbebc9634c386
Marcelo <Marcelo@DESKTOP-8BVE0KD>
2022-06-07 20:55:39 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio_Banco/Banco.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Banco
+{
+    internal class Banco
+    {
+        public int Conta { get; private set; }
+        public string Titular { get; private set; }
+        public double Saldo { get;private set; }
+
+        double Taxa = 5;
+        public Banco(int conta, string titular, double saldo)
+        {
+            Conta = conta;
+            Titular = titular;
+            Saldo = saldo;
+        }
+
+        public Banco(int conta, string titular)
+        {
+            Conta = conta;
+            Titular = titular;
+        }
+        
+        public void Deposito(double dp)
+        {
+            Saldo += dp;
+        }
+        public void Saque(double saque)
+        {
+            Saldo = (Saldo - saque) - Taxa;
+        }
+        public override string ToString()
+        {
+            return "Conta " + Conta + ", Titular: " + Titular + ", Saldo: " + Saldo.ToString("f2", CultureInfo.InvariantCulture);
+        }
+    }
+    
+}
---
+++ Exercicio_Banco/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o numero da conta: ");
+            int conta = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Hávera deposito inicial (s/n): ");
+            string inicial = Console.ReadLine();
+
+            Banco b = new Banco(conta, titular);
+
+            if (inicial == "s")
+            {
+                Console.Write("Entre com o valor do deposito: ");
+                double dp = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                b.Deposito(dp);
+                Console.WriteLine("Dados da conta: ");
+                Console.WriteLine(b);
+            }
+            else
+            {
+                Console.WriteLine("Dados da conta: ");
+                Console.WriteLine(b);
+            }
+
+            
+            Console.Write("Entre com valor para deposito: ");
+            double dpp = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b.Deposito(dpp);
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(b);
+            Console.Write("Entre com valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b.Saque(saque);
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(b);
+            
+        }
+    }
+}
